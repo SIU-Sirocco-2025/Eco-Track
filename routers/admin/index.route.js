f1edd6bce246325ad94b622d76d538b8e7698c50
SmallChicken2k5 <thp.gia@gmail.com>
2025-11-24 00:39:40 +0700

    login admin

--- routers/admin/index.route.js
+++ routers/admin/index.route.js
@@ -3,13 +3,19 @@ const aqiRoute = require('./aqi.route');
 const weatherRoute = require('./weather.route');
 const usersRoute = require('./users.route');
 const userRoute = require('./user.route');
-
+const authRoute = require('./auth.route')
+const { requireAuth } = require('../../middlewares/admin.middleware');
 
 module.exports = (app) => {
-    const PATH_ADMIN = app.locals.prefixAdmin; 
-    app.use(PATH_ADMIN + '/dashboard', dashboardRoute);
-    app.use(PATH_ADMIN + '/aqi', aqiRoute);
-    app.use(PATH_ADMIN + '/weather', weatherRoute);
-    app.use(PATH_ADMIN + '/users', usersRoute);
-    app.use(PATH_ADMIN + '/user', userRoute);
+    const PATH_ADMIN = app.locals.prefixAdmin;
+    
+    // Route không cần auth
+    app.use(PATH_ADMIN + '/auth', authRoute);
+    
+    // Áp dụng middleware cho các route cần auth
+    app.use(PATH_ADMIN + '/dashboard', requireAuth, dashboardRoute);
+    app.use(PATH_ADMIN + '/aqi', requireAuth, aqiRoute);
+    app.use(PATH_ADMIN + '/weather', requireAuth, weatherRoute);
+    app.use(PATH_ADMIN + '/users', requireAuth, usersRoute);
+    app.use(PATH_ADMIN + '/user', requireAuth, userRoute);
 };
